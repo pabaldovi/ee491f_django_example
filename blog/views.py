30af10510d8b962cce2215c4e3eef834a1fd8869
paulobaldovi <baldovipaulo@gmail.com>
2022-06-19 10:15:45 -1000

    add CRUD operations

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    model = Post
+
+class CreateView(generic.edit.CreateView):
+    template_name = 'blog/create.html'
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:index')
+
+class DetailView(generic.DetailView):
+    template_name = 'blog/detail.html'
+    model = Post
+
+class UpdateView(generic.edit.UpdateView):
+    template_name = 'blog/update.html'
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:index')
+
+class DeleteView(generic.edit.DeleteView):
+    template_name = 'blog/delete.html'
+    model = Post
+    success_url = reverse_lazy('blog:index')
